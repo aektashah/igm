427df0a36433ee353ad907c41c83d4a715f09edf
Instagram Map <igm@jpegdev.win>
2017-11-09 20:38:13 +0000

    updated config

--- twim/config/dev.exs
+++ twim/config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :twim, TwimWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 9000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
@@ -52,7 +52,7 @@ config :phoenix, :stacktrace_depth, 20
 config :twim, Twim.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "faijiMa7eeja",
+  password: "Soniczelda2",
   database: "twim_dev",
   hostname: "localhost",
   pool_size: 10
